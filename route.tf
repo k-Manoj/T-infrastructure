47ef941034c13952cd40e13791b80ddcd9a012ea
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2023-05-24 11:40:19 +0530

    Update route.tf

--- route.tf
+++ route.tf
@@ -11,6 +11,9 @@ resource "aws_route_table" "terraform-public" {
   }
 }
 
+resource "aws_route_table" "terraform-private-1" {
+  vpc_id = aws_vpc.default.id
+}
 resource "aws_route_table_association" "terraform-private-1" {
   subnet_id      = aws_subnet.subnet1-private.id
   route_table_id = aws_route_table.terraform-private-1.id
